862efcde1808d06a81714e07069b22451c307c7a
cizrage <thu.ya.a@gmail.com>
2016-11-13 11:56:48 +0800

    10 - Saving Collections

--- FirstHibernate/src/org/thomas/java/HibernateTest.java
+++ FirstHibernate/src/org/thomas/java/HibernateTest.java
@@ -16,10 +16,8 @@ public class HibernateTest {
 		session.beginTransaction();
 		
 		UserDetails user = new UserDetails();
-//		user.setUserId(1);
 		user.setUserName("alexandra");
-		user.setUserAdd1("Rome");
-		user.setUserAdd2("Roman Empire");
+
 		Address address = new Address();
 		address.setBlock("12-12");
 		address.setCity("Singpaore city");
@@ -27,14 +25,8 @@ public class HibernateTest {
 		address.setPostalcode("600600");
 		address.setState("Singapore State");
 		address.setStreet("Downing Street");
-		user.setHomeAddress(address);
-		session.save(user);
+		user.getListOfAddresses().add(address);
 		
-		user = new UserDetails();
-//		user.setUserId(2);
-		user.setUserName("franz");
-		user.setUserAdd1("Austria");
-		user.setUserAdd2("Austra-Hungry Empire");
 		address = new Address();
 		address.setBlock("123-123");
 		address.setCity("Yangon city");
@@ -42,22 +34,23 @@ public class HibernateTest {
 		address.setPostalcode("B48787");
 		address.setState("SHukhinthar State");
 		address.setStreet("Mandalar Street");
-		user.setOfficeAddress(address);		
+		user.getListOfAddresses().add(address);		
+
 		session.save(user);
 		
 		user = new UserDetails();
-//		user.setUserId(3);
+		user.setUserName("franz");
+		user.setUserDesc("how now brown cow");
+		session.save(user);
+		
+		user = new UserDetails();
 		user.setUserName("winston");
-		user.setUserAdd1("London");
-		user.setUserAdd2("England, United Kingdom");		
 		user.setUserDOB(new Date());
 		session.save(user);
 
-//		user = new UserDetails();
-//		user.setUserName("xxx");
-//		user.setUserAdd1("Triple X");
-//		user.setUserAdd2("Hollywood");
-//		session.save(user);		
+		user = new UserDetails();
+		user.setUserName("xxx");
+		session.save(user);		
 		
 		session.getTransaction().commit();
 		session.close();
@@ -67,23 +60,6 @@ public class HibernateTest {
 		session = sessionFactory.openSession();
 		session.beginTransaction();
 		user = session.get(UserDetails.class, 1);
-		System.out.printf("%s %s %s %s %s\n", user.getUserId()
-				, user.getUserName()
-				, user.getUserAdd1()
-				, user.getUserAdd2()
-				, user.getUserCountry());
-		user = session.get(UserDetails.class, 2);
-		System.out.printf("%s %s %s %s %s\n", user.getUserId()
-				, user.getUserName()
-				, user.getUserAdd1()
-				, user.getUserAdd2()
-				, user.getUserCountry());
-		user = session.get(UserDetails.class, 3);
-		System.out.printf("%s %s %s %s %s\n", user.getUserId()
-				, user.getUserName()
-				, user.getUserAdd1()
-				, user.getUserAdd2()
-				, user.getUserCountry());		
 		
 	}
 
--- FirstHibernate/src/org/thomas/java/dto/UserDetails.java
+++ FirstHibernate/src/org/thomas/java/dto/UserDetails.java
@@ -1,10 +1,14 @@
 package org.thomas.java.dto;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.enterprise.inject.New;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,29 +21,16 @@ import javax.persistence.TemporalType;
 @Entity
 @Table (name="USER_DETAILS")
 public class UserDetails {
+	
 	@Id @GeneratedValue
 	private int userId;	
 	private String userName;
-	private String userAdd1;
-	private String userAdd2;
-	private String userCountry;
 	@Temporal (TemporalType.DATE)
 	private Date userDOB;
 	@Lob 
 	private String userDesc;
-	@Embedded
-	@AttributeOverrides ({
-		@AttributeOverride (name="street", column=@Column(name="HOME_STREET")),
-		@AttributeOverride (name="block", column=@Column(name="HOME_BLOCK")),
-		@AttributeOverride (name="city", column=@Column(name="HOME_CITY")),
-		@AttributeOverride (name="state", column=@Column(name="HOME_STATE")),
-		@AttributeOverride (name="country", column=@Column(name="HOME_COUNTRY")),
-		@AttributeOverride (name="postalcode", column=@Column(name="HOME_POSTALCODE"))			
-	})	
-	private Address officeAddress;
-	@Embedded
-	private Address homeAddress;
-	
+	@ElementCollection
+	private Set<Address> listOfAddresses = new HashSet<Address>();
 	
 	
 	public int getUserId() {
@@ -54,24 +45,6 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public String getUserAdd1() {
-		return userAdd1;
-	}
-	public void setUserAdd1(String userAdd1) {
-		this.userAdd1 = userAdd1;
-	}
-	public String getUserAdd2() {
-		return userAdd2;
-	}
-	public void setUserAdd2(String userAdd2) {
-		this.userAdd2 = userAdd2;
-	}
-	public String getUserCountry() {
-		return userCountry;
-	}
-	public void setUserCountry(String userCountry) {
-		this.userCountry = userCountry;
-	}
 	public Date getUserDOB() {
 		return userDOB;
 	}
@@ -84,18 +57,11 @@ public class UserDetails {
 	public void setUserDesc(String userDesc) {
 		this.userDesc = userDesc;
 	}
-	public Address getOfficeAddress() {
-		return officeAddress;
+	public Set<Address> getListOfAddresses() {
+		return listOfAddresses;
 	}
-	public void setOfficeAddress(Address officeAddress) {
-		this.officeAddress = officeAddress;
+	public void setListOfAddresses(Set<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
 	}
-	public Address getHomeAddress() {
-		return homeAddress;
-	}
-	public void setHomeAddress(Address homeAddress) {
-		this.homeAddress = homeAddress;
-	}
-	
 
 }
